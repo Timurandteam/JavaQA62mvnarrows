bd12e8a2962c2e2c14e6b8564ed6a4d31c8af6e1
Tima test <timmy12122022@gmail.com>
2023-07-12 01:42:57 +0700

    Initial commit

---
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -0,0 +1,61 @@
+package ru.netology.stats;
+
+public class StatsService {
+    public long sum(long[] sales) {
+        long totalSale = 0;
+        for (long sale : sales) {
+            totalSale += sale;
+        }
+        return totalSale;
+    }
+
+    public long average(long[] sales) {
+
+        return sum(sales) / sales.length;
+    }
+
+
+    public int monthMaximumSale(long[] sales) {
+        int monthMaximum = 0;
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] >= sales[monthMaximum]) {
+                monthMaximum = i;
+            }
+        }
+        return monthMaximum + 1;
+    }
+
+    public int monthMinimumSale(long[] sales) {
+        int monthMinimumSale = 0;
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] <= sales[monthMinimumSale]) {
+                monthMinimumSale = i;
+            }
+        }
+        return monthMinimumSale + 1;
+    }
+
+    public int salesBellowAverage(long[] sales) {
+        int counter = 0;
+        long averageSale = average(sales);
+        for (long sale : sales) {
+            if (sale < averageSale) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    public int salesAboveAverage(long[] sales) {
+        int counter = 0;
+        long averageSale = average(sales);
+        for (long sale : sales) {
+            if (sale > averageSale) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
